3c1eb7d2f28e3b610da7d7dc9ba5199ed64c6b99
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-05-14 19:07:08 -0700

    working on adding todo form

---
+++ src/components/TodoForm.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function TodoForm(){
+    const [ todo, setTodo ] = useState('');
+
+    return (
+        <form className="flex justify-center p-5">
+            <input 
+                type="text"
+                className="border-black border-solid border-2"
+                onChange={event => }
+            />
+        </form>
+    );
+}
